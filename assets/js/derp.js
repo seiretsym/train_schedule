8802293993cd6d93b207ce5b305962388896f10d
Kerwin Hy <kerwin.hy@gmail.com>
2019-09-13 17:44:25 -0700

    add firebase to derp.js

--- assets/js/derp.js
+++ assets/js/derp.js
@@ -1,7 +1,22 @@
+// Your web app's Firebase configuration
+var firebaseConfig = {
+apiKey: "AIzaSyA-pXG9MTK1x5pBfW1RUqEDpgXlbrpy8dY",
+authDomain: "trainschedule-bb548.firebaseapp.com",
+databaseURL: "https://trainschedule-bb548.firebaseio.com",
+projectId: "trainschedule-bb548",
+storageBucket: "",
+messagingSenderId: "491227092044",
+appId: "1:491227092044:web:1ce1a7e095d1f077ee90e9"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+// create pointer to firebase database
+var db = firebase.database();
 
 // function to submit new train info to database
 function submitTrain() {
-    
+
 }
 
 // submit button click event listener
