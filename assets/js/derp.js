5201ea6f3a67d65233d5e8954b1f44d6b578be02
Kerwin Hy <kerwin.hy@gmail.com>
2019-09-13 17:41:26 -0700

    add event listener for submit button to call function to submit new train information

---
+++ assets/js/derp.js
@@ -0,0 +1,14 @@
+
+// function to submit new train info to database
+function submitTrain() {
+    
+}
+
+// submit button click event listener
+$(document).on("click", "#submitTrain", function() {
+    // prevent page refresh
+    event.preventDefault();
+
+    // submit train input values
+    submitTrain();
+})
